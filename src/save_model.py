3ecfeb2813524e8675c7f67583a8c1029fc60dbe
shijupaul <shijuppaul@hotmail.com>
2026-03-23 20:28:59 +0000

    Saving model along with script

---
+++ src/save_model.py
@@ -0,0 +1,9 @@
+import joblib
+from train_model import get_model
+
+def save_model(model, filename):
+    joblib.dump(model, filename)
+    print(f"Model saved to {filename}")
+
+model = get_model('data/machine_failure_train.csv')
+save_model(model, 'models/rf_model.pkl')
